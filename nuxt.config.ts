d284f8dbe329737135246915dccef04596514a75
Kemboi Elvis <kemboielvis22@gmail.com>
2025-05-05 23:20:36 +0300

    feat: added shadcn components

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,13 +3,14 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
-
+  css: ["~/assets/css/tailwind.css"],
   modules: [
     "@nuxt/eslint",
     "@nuxt/fonts",
     "@nuxt/icon",
     "@nuxt/image",
     "@nuxt/scripts",
+    "shadcn-nuxt",
   ],
   vite: {
     plugins: [tailwindcss()],
