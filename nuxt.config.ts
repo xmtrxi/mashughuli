be226e8ecfbacddee526d031a736cc7914d50e39
Kemboi Elvis <kemboielvis22@gmail.com>
2025-05-05 23:04:46 +0300

    initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,17 @@
+import tailwindcss from "@tailwindcss/vite";
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: "2024-11-01",
+  devtools: { enabled: true },
+
+  modules: [
+    "@nuxt/eslint",
+    "@nuxt/fonts",
+    "@nuxt/icon",
+    "@nuxt/image",
+    "@nuxt/scripts",
+  ],
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
